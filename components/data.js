ac3dcfabefd497dd32fd2bff871759fe3effbe4e
sheikh-zafar <audios.shkzafar@gmail.com>
2023-10-01 19:25:05 +0400

    commit

--- components/data.js
+++ components/data.js
@@ -5,10 +5,10 @@ import {
   DevicePhoneMobileIcon,
   AdjustmentsHorizontalIcon,
   SunIcon,
-} from "@heroicons/react/24/solid";
+} from '@heroicons/react/24/solid';
 
-import benefitOneImg from "../public/img/benefit-one.png";
-import benefitTwoImg from "../public/img/benefit-two.png";
+import benefitOneImg from '../public/img/contracting/tallbuilding.jpeg';
+import benefitTwoImg from '../public/img/benefit-two.png';
 
 const benefitOne = {
   title: 'Highlight your benefits',
@@ -54,27 +54,26 @@ const benefitOne = {
 };
 
 const benefitTwo = {
-  title: "Offer more benefits here",
-  desc: "You can use this same layout with a flip image to highlight your rest of the benefits of your product. It can also contain an image or Illustration as above section along with some bullet points.",
+  title: 'Offer more benefits here',
+  desc: 'You can use this same layout with a flip image to highlight your rest of the benefits of your product. It can also contain an image or Illustration as above section along with some bullet points.',
   image: benefitTwoImg,
   bullets: [
     {
-      title: "Mobile Responsive Template",
-      desc: "Nextly is designed as a mobile first responsive template.",
+      title: 'Mobile Responsive Template',
+      desc: 'Nextly is designed as a mobile first responsive template.',
       icon: <DevicePhoneMobileIcon />,
     },
     {
-      title: "Powered by Next.js & TailwindCSS",
-      desc: "This template is powered by latest technologies and tools.",
+      title: 'Powered by Next.js & TailwindCSS',
+      desc: 'This template is powered by latest technologies and tools.',
       icon: <AdjustmentsHorizontalIcon />,
     },
     {
-      title: "Dark & Light Mode",
-      desc: "Nextly comes with a zero-config light & dark mode. ",
+      title: 'Dark & Light Mode',
+      desc: 'Nextly comes with a zero-config light & dark mode. ',
       icon: <SunIcon />,
     },
   ],
 };
 
-
-export {benefitOne, benefitTwo};
+export { benefitOne, benefitTwo };
